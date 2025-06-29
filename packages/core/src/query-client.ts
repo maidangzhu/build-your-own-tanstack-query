0374a6ca1edfd53ae4a87d0152563f4d0101c0b1
朱建业 <borisdunk@sina.com>
2025-06-29 15:02:09 +0800

    feat: query client

--- packages/core/src/query-client.ts
+++ packages/core/src/query-client.ts
@@ -1,1 +1,18 @@
 // TODO: 实现 QueryClient 类 
+import { QueryCache } from "./query-cache";
+
+export class QueryClient {
+  private queryCache: QueryCache;
+
+  constructor() {
+    this.queryCache = new QueryCache();
+  }
+
+  getQueryCache() {
+    return this.queryCache;
+  }
+
+  setQueryCache(queryCache: QueryCache) {
+    this.queryCache = queryCache;
+  }
+}
