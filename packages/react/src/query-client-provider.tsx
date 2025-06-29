6cdabfe4d3deffcdb35ce7cf2a1040d6a7778476
朱建业 <borisdunk@sina.com>
2025-06-29 14:57:10 +0800

    feat: query-client-provider

--- packages/react/src/query-client-provider.tsx
+++ packages/react/src/query-client-provider.tsx
@@ -1,1 +1,24 @@
 // TODO: 实现 QueryClientProvider 组件 
+
+import { createContext, useContext, useState } from "react";
+import { QueryClient } from "@the-tanstack-query/core";
+
+const QueryClientContext = createContext<QueryClient | null>(null);
+
+export const useQueryClient = () => {
+  const queryClient = useContext(QueryClientContext);
+  if (!queryClient) {
+    throw new Error('QueryClient not found');
+  }
+  return queryClient;
+}
+
+export const QueryClientProvider = ({children}: {children: React.ReactNode}) => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientContext.Provider value={queryClient}>
+      {children}
+    </QueryClientContext.Provider>
+  )
+}
